697fae54cde42d32303f89fc78a6955ba8a6b3d3
Sadiq Goni <sadiqgoni13@gmail.com>
2026-05-19 12:18:01 +0100

    Add salary templates for Contiss II and Conuass
    - Created new Excel files for salary templates: salary_template_contiss_ii.xlsx and salary_template_conuass.xlsx.
    - Each file contains structured data for salary calculations, formatted for use in payroll processing.

--- app/Imports/SalaryTemplate.php
+++ app/Imports/SalaryTemplate.php
@@ -21,32 +21,33 @@ class SalaryTemplate implements ToModel, WithHeadingRow
     }
     public function model(array $row)
     {
-        // dd($row['step2']);
+        $step = fn ($key) => $row[$key] ?? 0;
+
         return new SalaryStructureTemplate([
             'salary_structure_id'=>$this->data,
             'grade_level'=>$row['grade_level_from'],
             // 'grade_level_to'=>$row['grade_level_to'],
             'no_of_grade_steps'=>$row['no_of_grade_steps'],
-            'Step1'=>$row['step1'],
-            'Step2'=>$row['step2'],
-            'Step3'=>$row['step3'],
-            'Step4'=>$row['step4'],
-            'Step5'=>$row['step5'],
-            'Step6'=>$row['step6'],
-            'Step7'=>$row['step7'],
-            'Step8'=>$row['step8'],
-            'Step9'=>$row['step9'],
-            'Step10'=>$row['step10'],
-            'Step11'=>$row['step11'],
-            'Step12'=>$row['step12'],
-            'Step13'=>$row['step13'],
-            'Step14'=>$row['step14'],
-            'Step15'=>$row['step15'],
-            'Step16'=>$row['step16'],
-            'Step17'=>$row['step17'],
-            'Step18'=>$row['step18'],
-            'Step19'=>$row['step19'],
-            'Step20'=>$row['step20'],
+            'Step1'=>$step('step1'),
+            'Step2'=>$step('step2'),
+            'Step3'=>$step('step3'),
+            'Step4'=>$step('step4'),
+            'Step5'=>$step('step5'),
+            'Step6'=>$step('step6'),
+            'Step7'=>$step('step7'),
+            'Step8'=>$step('step8'),
+            'Step9'=>$step('step9'),
+            'Step10'=>$step('step10'),
+            'Step11'=>$step('step11'),
+            'Step12'=>$step('step12'),
+            'Step13'=>$step('step13'),
+            'Step14'=>$step('step14'),
+            'Step15'=>$step('step15'),
+            'Step16'=>$step('step16'),
+            'Step17'=>$step('step17'),
+            'Step18'=>$step('step18'),
+            'Step19'=>$step('step19'),
+            'Step20'=>$step('step20'),
         ]);
     }
     public function batchSize(): int
